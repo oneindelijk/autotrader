4fad5cac6352afaa6afe035e67ac0054eaacc625
oneindelijk <oneindelijk@gmail.com>
2021-01-26 16:26:09 +0100

    add screebshots

--- pyTrade/portofolio/models.py
+++ pyTrade/portofolio/models.py
@@ -1,35 +1,59 @@
 
 from django.db import models
 import datetime
-from stocktools import download_ticker_dataframe
+from .stocktools import download_ticker_dataframe
 # Create your models here.
 
 class Company(models.Model):
 
     # portofolio = models.ForeignKey(Portofolio, on_delete = models.CASCADE)
-    symbol = models.CharField('Symbol', max_length = 200) 
-    name = models.CharField('Symbol', max_length = 200)  
+    symbol = models.CharField('Symbol', max_length = 200)
+    name = models.CharField('Name', max_length = 200)   
+    symbol_cqs = models.CharField('CQS Symbol', max_length = 200, null=True, blank=True) 
+    symbol_nasdaq = models.CharField('NASDAQ Symbol', max_length = 200, null=True, blank=True)
+    etf = models.BooleanField('ETF', max_length = 200, null=True, blank=True)  
+    market_category = models.CharField('Market Category', null=True, blank=True, max_length = 200) 
     last_sale = models.FloatField('Last Sale', null=True, blank=True)  
     net_change = models.FloatField('Net Change', null=True, blank=True) 
     relative_change = models.FloatField('% Change', null=True, blank=True)  
     market_cap = models.FloatField('Market Cap', null=True, blank=True)  
-    country = models.CharField('Country', max_length = 200)  
-    ipo_year = models.CharField('IPO Year', max_length = 200) 
+    country = models.CharField('Country', null=True, blank=True, max_length = 200)  
+    ipo_year = models.CharField('IPO Year', null=True, blank=True, max_length = 200) 
     volume = models.FloatField('Volume', null=True, blank=True) 
-    sector = models.CharField('Industry', max_length = 200) 
-    industry = models.CharField('Symbol', max_length = 200)
+    sector = models.CharField('Sector', null=True, blank=True, max_length = 200) 
+    industry = models.CharField('Industry', null=True, blank=True, max_length = 200)
     favorite = models.BooleanField('Favorite', default = False)
     watched = models.BooleanField('Watched', default = False)
     marked = models.BooleanField('Marked', default = False)
     bought = models.BooleanField('Bought', default = False)
-    sell = models.BooleanField('Sell', default = True)
+    sell = models.BooleanField('Sell', default = True)  # Sell if conditions are right, 
+                                                        # specifically hold back from selling when untrue
 
     def __str__(self):
         return f"{self.name}"
 
-    def refresh_companies(self):
+    def refresh_companies():
         ''' refresh the list of companies listings on nasdaq '''
         dataframe = download_ticker_dataframe()
+        C = Company()
+        field_links = [
+                    #    ('Nasdaq Traded',None),
+                       ('Security Name',C.name), 
+                    #    ('Listing Exchange',None),
+                       ('Market Category',C.market_category),
+                       ('ETF',C.etf),
+                    #    'Round Lot Size', 'Test Issue', 'Financial Status', 
+                       ('CQS Symbol',C.symbol_cqs),
+                       ('NASDAQ Symbol',C.symbol_nasdaq),
+                    # 'NextShares'
+                       ]
+        index_lookup = list(dataframe.keys())
+        for df_row in dataframe.itertuples():
+            C = Company.objects.create(symbol = df_row.index())
+            for df_field, table_col in field_links:
+                table_col = df_row[index_lookup.index(df_field) + 1]
+            C.save()
+
 
     
 class StockData(models.Model):
