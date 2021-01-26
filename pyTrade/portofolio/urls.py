e45db83fdec444224fe694a9b65161eab2d0d493
oneindelijk <oneindelijk@gmail.com>
2021-01-26 17:59:59 +0100

    clean up base.html

--- pyTrade/portofolio/urls.py
+++ pyTrade/portofolio/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'portofolio'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('company-listing/', views.CompanyListing.as_view(), name = 'companies')
     # path('<int:wallet_id>/', views.detail, name='detail'),  
     # path('operations/<int:balance_id>/', views.balance_detail, name='balance'),
     # path('<int:wallet_id>/add_valuta', views.add_valuta, name='add_valuta'),  
--- pyTrade/portofolio/views.py
+++ pyTrade/portofolio/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 
-from .models import Portofolio
+from .models import Portofolio, Company
+
+def get_pages_list(active_id):
+    pages = ['index', 'companies']
+    Pages = []
+    for i,p in enumerate(pages):
+        Pages.append({'active': i + 1 == active_id,
+                      'id': i + 1, 
+                      'link':'portofolio:' + p,
+                      'title': p[0].upper() + p[1:]
+                      })
+    return Pages
+
 
 def index(request):
-    pass
-    context = {'Portofolio': Portofolio}
+    pages_list = get_pages_list(1)
+    context = {'Portofolio': Portofolio, 'pages_list': pages_list}
     return render(request, 'portofolio/index.html', context)
 
+class CompanyListing(generic.ListView):
+    template_name = 'portofolio/companies.html'
+    model = Company
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pages_list = get_pages_list(2)
+        context.update({'pages_list': pages_list})
+        print(context)
+        return context
